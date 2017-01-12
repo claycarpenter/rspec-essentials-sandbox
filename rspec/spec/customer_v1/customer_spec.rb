b1ae8d116c267740e5dc46791eb5f5553449066f
Clay Carpenter <claycarpenter@gmail.com>
2017-01-12 09:24:17 -0500

    First iteration of Customer with spec and custom matchers

---
+++ rspec/spec/customer_v1/customer_spec.rb
@@ -0,0 +1,55 @@
+require 'rspec'
+require 'rspec/expectations'
+
+require 'customer_v1/customer'
+
+RSpec::Matchers.define :be_discounted do |product, expected|
+  match do |customer|
+    @actual = customer.discount_amount_for(product)
+
+    customer.discount_amount_for(product) == expected
+  end
+
+  failure_message do |actual|
+    "expected #{product} discount of #{expected}, got #{actual}"
+  end
+end
+
+class HaveDiscountOf
+  def initialize(expected)
+    @expected = expected
+  end
+
+  def matches?(customer)
+    @actual = customer.discount_amount_for(@product)
+
+    @actual == @expected
+  end
+
+  def for(product)
+    @product = product
+    self
+  end
+
+  def failure_message
+    "expected #{@product} discount of #{@expected}, got #{@actual}"
+  end
+end
+
+describe "product discount" do
+  def have_discount_of(discount)
+    HaveDiscountOf.new(discount)
+  end
+
+  let(:product) { 'foo123' }
+  let(:discounts) { { product => 0.1 } }
+  subject(:customer) { CustomerV1::Customer.new(discounts: discounts) }
+
+  it "detects when customer has a discount" do
+    expect(customer).to be_discounted(product, 0.1)
+  end
+
+  it "detects when customer has a discount (class matcher)" do
+    expect(customer).to have_discount_of(0.1).for(product)
+  end
+end
